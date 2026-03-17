8b8104f0916fe8fe409002fcbd9411cac0f662af
Riley Rockwell <rileyrockwell@gmail.com>
2026-03-17 09:54:49 +0000

    updates

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 class Nonsense(object):
     
-    def __init__(self, dm1, dm2, dm3):
+    def __init__(self, dm1, dm2, dm3, dm4):
         self.one = dm1
         self.two = dm2
         self.three = dm3
+        self.four = dm4
 
 
-instance = Nonsense('some', 'bullshit', 'somewhere')
+instance = Nonsense('some', 'bullshit', 'happening', 'somewhere')
 
 print(instance.one)
 print(instance.two)
 print(instance.three)
+print(instance.four)
